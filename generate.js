a19903323d8fba726bdcd8316cee8b97a8cfcad0
Jesper van den Ende <jespertheend@gmail.com>
2022-08-30 12:09:09 +0200

    Generate types using the unstable flag

--- generate.js
+++ generate.js
@@ -5,7 +5,7 @@ import { setCwd } from "https://deno.land/x/chdir_anywhere@v0.0.2/mod.js";
 setCwd();
 
 const p = Deno.run({
-  cmd: ["deno", "types"],
+  cmd: ["deno", "types", "--unstable"],
   stdout: "piped",
 });
 
