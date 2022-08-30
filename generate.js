8fda672b6612cb63f8570222facb799ed20a215a
Jesper van den Ende <jespertheend@gmail.com>
2022-08-30 12:08:03 +0200

    Fix error when deno-types directory doesn't exist

--- generate.js
+++ generate.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env -S deno run --allow-run --allow-read --allow-write
 
+import { ensureDir } from "https://deno.land/std@0.153.0/fs/mod.ts";
 import { setCwd } from "https://deno.land/x/chdir_anywhere@v0.0.2/mod.js";
 setCwd();
 
@@ -9,4 +10,5 @@ const p = Deno.run({
 });
 
 const typesContent = await p.output();
+await ensureDir("./package/deno-types/");
 await Deno.writeFile("./package/deno-types/index.d.ts", typesContent);
